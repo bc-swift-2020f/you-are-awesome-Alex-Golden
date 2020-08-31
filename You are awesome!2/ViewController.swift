1f497a535d64bb3bb80d6570b25bc0d6f140a909
Alex Golden <goldenah@bc.edu>
2020-08-31 13:45:07 -0400

    commit 2.1

---
+++ You are awesome!2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  You are awesome!2
+//
+//  Created by Alex Golden on 8/31/20.
+//  Copyright © 2020 Alex Golden. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
