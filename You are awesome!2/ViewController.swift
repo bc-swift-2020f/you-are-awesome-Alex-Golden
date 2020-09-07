ddeb4f5a4a7c24be14e202f3434dab72db60bbad
Alex Golden <goldenah@bc.edu>
2020-09-07 13:39:53 -0400

    Resize

--- You are awesome!2/ViewController.swift
+++ You are awesome!2/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var imageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        messageLabel.text = " "
        print("👍viewDidLoad has run")
     }
 
