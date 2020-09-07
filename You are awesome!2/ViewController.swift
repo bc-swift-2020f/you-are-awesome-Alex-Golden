0ef1c095af6ec4e82d2b21064a5757f2566111b5
Alex Golden <goldenah@bc.edu>
2020-09-07 14:48:06 -0400

    ifs (bug)

--- You are awesome!2/ViewController.swift
+++ You are awesome!2/ViewController.swift
@@ -21,9 +21,21 @@ class ViewController: UIViewController {
 
     @IBAction func messageButtonPressed(_ sender: UIButton) {
         print("😎The message button was pressed")
-        messageLabel.text = "You are awesome!"
-        imageView.image = UIImage(named: "image0")
+    let awesomeMessage = "You are awesome!"
+        let greatMessage = "You are great!"
+        let bombMessage = "You are da bomb!"
         
+        if messageLabel.text == awesomeMessage {
+            messageLabel.text = greatMessage
+            imageView.image = UIImage(named: "image1")
+            
+            else if messageLabel.text == greatMessage{
+                messageLabel.text = bombMessage
+                imageView.image = UIImage(named: "image2")
+            }
+        } else {messageLabel.text = awesomeMessage
+            imageView.image = UIImage(named: "image0") }
     }
+    
 }
 
